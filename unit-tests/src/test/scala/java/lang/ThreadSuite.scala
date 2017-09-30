ee52087da9fb04194a924032a75ada962a8b9068
Valdis <valdisxp1@gmail.com>
2017-09-30 19:28:34 +0200

    test exceptions

--- unit-tests/src/test/scala/java/lang/ThreadSuite.scala
+++ unit-tests/src/test/scala/java/lang/ThreadSuite.scala
@@ -32,4 +32,33 @@ object ThreadSuite extends tests.Suite {
     assertEquals(shared,1)
   }
 
+  def withExceptionHandler[U](handler: Thread.UncaughtExceptionHandler)(f: => U): U = {
+    val oldHandler = Thread.getDefaultUncaughtExceptionHandler
+    try {
+      f
+    } finally {
+      Thread.setDefaultUncaughtExceptionHandler(oldHandler)
+    }
+  }
+
+  test("Exceptions in Threads should be handled"){
+    val exception = new NullPointerException("There must be a null somewhere")
+    val thread = new Thread(new Runnable {
+      def run(): Unit = {
+        throw exception
+      }
+    })
+    var wasException = false
+    withExceptionHandler(new Thread.UncaughtExceptionHandler{
+      def uncaughtException(t: Thread, e: Throwable) = {
+        assertEquals(t, thread)
+        assertEquals(e, exception)
+        wasException = true
+      }
+    }){
+      thread.start()
+      Thread.sleep(100)
+    }
+    assert(wasException)
+  }
 }
