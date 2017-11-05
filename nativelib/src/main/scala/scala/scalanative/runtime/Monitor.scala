99ca8cb4ef92b13ec4e523e165bf22021e7d6003
Valdis <valdisxp1@gmail.com>
2017-11-05 17:20:25 +0100

    add a field to objects (rebased)

--- nativelib/src/main/scala/scala/scalanative/runtime/Monitor.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/Monitor.scala
@@ -3,9 +3,9 @@ package scala.scalanative.runtime
 import scala.scalanative.posix.pthread._
 import scala.scalanative.posix.errno.EBUSY
 import scala.scalanative.posix.sys.types.{pthread_mutex_t, pthread_mutexattr_t}
-import scala.scalanative.native._
+import scala.scalanative.native.{Ptr, _}
 import scala.scalanative.native.stdlib.malloc
-final class Monitor private () {
+final class Monitor private[runtime] () {
 
   private val mutexPtr: Ptr[pthread_mutex_t] = malloc(pthread_mutex_t_size)
     .asInstanceOf[Ptr[pthread_mutex_t]]
@@ -45,6 +45,25 @@ object Monitor {
     pthread_mutexattr_t_size).asInstanceOf[Ptr[pthread_mutexattr_t]]
   pthread_mutexattr_init(mutexAttrPtr)
   pthread_mutexattr_settype(mutexAttrPtr, PTHREAD_MUTEX_RECURSIVE)
+
+  private[runtime] val monitorCreationMutexPtr : Ptr[pthread_mutex_t] = malloc(pthread_mutex_t_size)
+    .asInstanceOf[Ptr[pthread_mutex_t]]
+  pthread_mutex_init(monitorCreationMutexPtr, Monitor.mutexAttrPtr)
+
+  def apply(x: java.lang.Object): Monitor = {
+    val o = x.asInstanceOf[_Object]
+    if (o.__monitor != null) {
+      o.__monitor
+    } else {
+      try {
+        pthread_mutex_lock(monitorCreationMutexPtr)
+        o.__monitor = new Monitor()
+        o.__monitor
+      } finally {
+        pthread_mutex_unlock(monitorCreationMutexPtr)
+      }
+    }
+  }
 
   val global: Monitor = new Monitor()
 }
