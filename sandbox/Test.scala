5154eb56a88fdb24793bd7c981d781f58be75144
Valdis <valdisxp1@gmail.com>
2017-11-28 12:05:25 +0100

    example

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,5 +1,23 @@
 object Test {
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
+    Console.err.println("1")
+    System.err.println("1b")
+    Console.withErr(System.out){
+      Console.err.println("2a")
+      System.err.println("2b")
+    }
+    Console.err.println("2a")
+    System.err.println("2b")
+
+    // does not compile: cannot link: @java.lang.System$::setErr_java.io.PrintStream_unit
+    System.setErr(System.out)
+    Console.err.println("3a")
+    System.err.println("3b")
+
+    // does not compile: reassignment to val
+    System.err = System.out
+    Console.err.println("4a")
+    System.err.println("4b")
   }
 }
