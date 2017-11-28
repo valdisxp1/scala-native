d4bd0007325861a585bba25748b4bc75672ed3fb
Valdis <valdisxp1@gmail.com>
2017-11-28 17:53:37 +0100

    just check Thread.yield does not crash

--- unit-tests/src/test/scala/java/lang/ThreadSuite.scala
+++ unit-tests/src/test/scala/java/lang/ThreadSuite.scala
@@ -35,6 +35,15 @@ object ThreadSuite extends tests.MultiThreadSuite {
     assert(thread.getPriority == 2)
   }
 
+  test("Thread.yield does not crash") {
+    Thread.`yield`()
+    val thread = new Thread {
+      override def run() = Thread.`yield`()
+    }
+    thread.start()
+    thread.join()
+  }
+
   test("GC should not crash with multiple threads") {
     val muncher1 = new MemoryMuncher(10000)
     val muncher2 = new MemoryMuncher(10000)
