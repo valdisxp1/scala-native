70707bdfaef0134f2afb06c56f8e204dee72d9ef
Valdis <valdisxp1@gmail.com>
2017-11-28 18:39:58 +0100

    test  ThreadGroup.interrupt

--- unit-tests/src/test/scala/java/lang/ThreadGroupSuite.scala
+++ unit-tests/src/test/scala/java/lang/ThreadGroupSuite.scala
@@ -3,7 +3,7 @@ package java.lang
 object ThreadGroupSuite extends tests.MultiThreadSuite {
   test("ThreadGroup.checkAccess does not crash") {
     val mainThreadGroup = Thread.currentThread().getThreadGroup
-    val threadGroup = new ThreadGroup("abc")
+    val threadGroup     = new ThreadGroup("abc")
 
     mainThreadGroup.checkAccess()
     threadGroup.checkAccess()
@@ -16,5 +16,42 @@ object ThreadGroupSuite extends tests.MultiThreadSuite {
     }
     thread.start()
     thread.join()
+  }
+
+  test("ThreadGroup.interrupt should interrupt sleep for all threads") {
+    var fail = false
+    class SleepyThread(group: ThreadGroup, name: String)
+        extends Thread(group, name) {
+      override def run() = {
+        expectThrows(classOf[InterruptedException], {
+          Thread.sleep(10 * eternity)
+          fail = true
+        })
+      }
+    }
+    val group    = new ThreadGroup("group")
+    val subgroup = new ThreadGroup(group, "subgroup")
+    val threads = scala.Seq(
+      new SleepyThread(group, "G-1"),
+      new SleepyThread(group, "G-2"),
+      new SleepyThread(group, "G-3"),
+      new SleepyThread(subgroup, "SG-1"),
+      new SleepyThread(subgroup, "SG-2")
+    )
+    threads.foreach(_.start())
+    threads.foreach { thread =>
+      eventuallyEquals(
+        label = thread.toString + "is in Thread.State.TIMED_WAITING")(
+        Thread.State.TIMED_WAITING,
+        thread.getState)
+    }
+    group.interrupt()
+    threads.foreach { thread =>
+      eventuallyEquals(
+        label = thread.toString + "is in Thread.State.TERMINATED")(
+        Thread.State.TERMINATED,
+        thread.getState)
+    }
+    assertNot(fail)
   }
 }
