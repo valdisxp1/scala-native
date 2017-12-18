f018e260886a4a9e373329dcd6de22ca814f507d
Valdis <valdisxp1@gmail.com>
2017-12-18 20:39:40 +0100

    benchmark: pi calculation with monte carlo method using futures

---
+++ benchmarks/src/main/scala/montecarlo/MonteCarloPiFutures.scala
@@ -0,0 +1,33 @@
+package montecarlo
+
+import benchmarks.{BenchmarkRunningTime, ShortRunningTime}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.Future
+import scala.concurrent.forkjoin.ThreadLocalRandom
+
+class MonteCarloPiFutures extends benchmarks.Benchmark[Double] {
+
+  override val runningTime: BenchmarkRunningTime = ShortRunningTime
+
+  override def run(): Double = {
+    val points = 200000
+    val futures: Seq[Future[Boolean]] = (1 to points).map{
+      _ =>
+        Future{
+          val random = ThreadLocalRandom.current()
+          val x = random.nextDouble()
+          val y = random.nextDouble()
+          x * x + y * y < 1
+        }
+    }
+    val countFuture:Future[Int] = Future.fold(futures)(0) {
+      (sum, b) => sum + (if(b) 1 else 0)
+    }
+    val count = countFuture.value.get.get
+    4.0 * count / points
+  }
+
+  override def check(result: Double): Boolean =
+    (Math.PI - result) * (Math.PI - result) < 0.01
+}
