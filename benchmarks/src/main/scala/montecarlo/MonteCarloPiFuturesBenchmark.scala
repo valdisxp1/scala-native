2ebb831882f7c257ebe44943df954b3a1109dee5
Valdis <valdisxp1@gmail.com>
2017-12-18 21:45:12 +0100

    cannot easily get the value

--- benchmarks/src/main/scala/montecarlo/MonteCarloPiFuturesBenchmark.scala
+++ benchmarks/src/main/scala/montecarlo/MonteCarloPiFuturesBenchmark.scala
@@ -1,9 +1,10 @@
 package montecarlo
 
 import benchmarks.{BenchmarkRunningTime, ShortRunningTime}
-import scala.concurrent.ExecutionContext.Implicits.global
 
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.concurrent.forkjoin.ThreadLocalRandom
 
 class MonteCarloPiFuturesBenchmark extends benchmarks.Benchmark[Double] {
@@ -23,7 +24,26 @@ class MonteCarloPiFuturesBenchmark extends benchmarks.Benchmark[Double] {
     val countFuture: Future[Int] = Future.fold(futures)(0) { (sum, b) =>
       sum + (if (b) 1 else 0)
     }
-    val count = countFuture.value.get.get
+
+    var count = -1
+    val mutex = new Object
+
+    countFuture.foreach {
+      number: Int =>
+        mutex.synchronized {
+          count = number
+          mutex.notify()
+        }
+    }
+
+    if (count == -1) {
+      mutex.synchronized {
+        if (count == -1) {
+          mutex.wait()
+        }
+      }
+    }
+
     4.0 * count / points
   }
 
