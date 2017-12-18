2502c812082b411db0ce45de62dd1d82f5c076c2
Valdis <valdisxp1@gmail.com>
2017-12-18 16:14:46 +0100

    fix atomic reference

--- javalib/src/main/scala/java/util/concurrent/atomic/AtomicReference.scala
+++ javalib/src/main/scala/java/util/concurrent/atomic/AtomicReference.scala
@@ -1,46 +1,45 @@
 package java.util.concurrent.atomic
 
+import scala.scalanative.native.{CCast, CLong}
 import scala.scalanative.runtime.CAtomicLong
 
 class AtomicReference[T <: AnyRef](private[this] var value: T)
     extends Serializable {
 
-  def this() = this(0L.asInstanceOf[T])
+  def this() = this(null.asInstanceOf[T])
 
-  private[this] val inner = CAtomicLong(value.asInstanceOf[Long])
+  private[this] val inner = CAtomicLong(value.asInstanceOf[AnyRef].cast[CLong])
 
-  final def get(): T = inner.load()
+  final def get(): T = inner.load().cast[AnyRef].asInstanceOf[T]
 
   final def set(newValue: T): Unit =
-    inner.store(newValue.asInstanceOf[Long])
+    inner.store(newValue.asInstanceOf[AnyRef].cast[CLong])
 
   final def lazySet(newValue: T): Unit =
-    inner.store(newValue.asInstanceOf[Long])
+    inner.store(newValue.asInstanceOf[AnyRef].cast[CLong])
 
   final def compareAndSet(expect: T, update: T): Boolean = {
     inner
-      .compareAndSwapStrong(expect.asInstanceOf[Long],
-                            update.asInstanceOf[Long])
+      .compareAndSwapStrong(expect.asInstanceOf[AnyRef].cast[CLong],
+                            update.asInstanceOf[AnyRef].cast[CLong])
       ._1
   }
 
   final def weakCompareAndSet(expect: T, update: T): Boolean =
     inner
-      .compareAndSwapWeak(expect.asInstanceOf[Long], update.asInstanceOf[Long])
+      .compareAndSwapWeak(expect.asInstanceOf[AnyRef].cast[CLong],
+                          update.asInstanceOf[AnyRef].cast[CLong])
       ._1
 
   final def getAndSet(newValue: T): T = {
     val old = inner.load()
-    inner.store(newValue.asInstanceOf[Long])
-    old
+    inner.store(newValue.asInstanceOf[AnyRef].cast[CLong])
+    old.cast[AnyRef].asInstanceOf[T]
   }
 
   override def toString(): String =
     String.valueOf(value)
 
-  private implicit def toLong(e: T): Long = e.asInstanceOf[Long]
-
-  private implicit def toRef(l: Long): T = l.asInstanceOf[T]
 }
 
 object AtomicReference {
