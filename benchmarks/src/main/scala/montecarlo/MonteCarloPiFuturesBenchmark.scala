ea238ee8dcc95f97a42c81a8068340ed5da9b788
Valdis <valdisxp1@gmail.com>
2017-12-18 21:07:48 +0100

    benchmark is now discoverable

--- benchmarks/src/main/scala/montecarlo/MonteCarloPiFuturesBenchmark.scala
+++ benchmarks/src/main/scala/montecarlo/MonteCarloPiFuturesBenchmark.scala
@@ -6,23 +6,22 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.forkjoin.ThreadLocalRandom
 
-class MonteCarloPiFutures extends benchmarks.Benchmark[Double] {
+class MonteCarloPiFuturesBenchmark extends benchmarks.Benchmark[Double] {
 
   override val runningTime: BenchmarkRunningTime = ShortRunningTime
 
   override def run(): Double = {
     val points = 200000
-    val futures: Seq[Future[Boolean]] = (1 to points).map{
-      _ =>
-        Future{
-          val random = ThreadLocalRandom.current()
-          val x = random.nextDouble()
-          val y = random.nextDouble()
-          x * x + y * y < 1
-        }
+    val futures: Seq[Future[Boolean]] = (1 to points).map { _ =>
+      Future {
+        val random = ThreadLocalRandom.current()
+        val x      = random.nextDouble()
+        val y      = random.nextDouble()
+        x * x + y * y < 1
+      }
     }
-    val countFuture:Future[Int] = Future.fold(futures)(0) {
-      (sum, b) => sum + (if(b) 1 else 0)
+    val countFuture: Future[Int] = Future.fold(futures)(0) { (sum, b) =>
+      sum + (if (b) 1 else 0)
     }
     val count = countFuture.value.get.get
     4.0 * count / points
