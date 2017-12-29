76daf1bc38b63013c00fb5d5908bc92affece988
Valdis <valdisxp1@gmail.com>
2017-12-29 12:03:13 +0200

    yield when testing eventually clauses

--- unit-tests/src/main/scala/tests/MultiThreadSuite.scala
+++ unit-tests/src/main/scala/tests/MultiThreadSuite.scala
@@ -54,6 +54,7 @@ trait MultiThreadSuite extends Suite {
       if (p) {
         continue = false
       }
+      Thread.`yield`()
       Thread.sleep(recheckEvery)
     }
     if (current <= deadline) {
@@ -99,6 +100,7 @@ trait MultiThreadSuite extends Suite {
         lastValueTs = current
         lastValue = currentValue
       }
+      Thread.`yield`()
       Thread.sleep(recheckEvery)
     }
     if (reached) {
