799d2a36ab99bedeba31ed0a88e7d59273f73ad6
Valdis <valdisxp1@gmail.com>
2017-12-29 12:24:31 +0200

    More workarounds

--- unit-tests/src/test/scala/java/lang/ThreadGroupSuite.scala
+++ unit-tests/src/test/scala/java/lang/ThreadGroupSuite.scala
@@ -287,6 +287,9 @@ object ThreadGroupSuite extends tests.MultiThreadSuite {
   }
 
   test("*DEPRECATED*  ThreadGroup.suspend and resume should affect all threads") {
+    //XXX workaround making sure GC does not happen when suspend tests are running
+    System.gc()
+
     val structure = new Structure[Counter] {
       def makeTread(group: ThreadGroup, name: String) = new Counter(group, name)
     }
