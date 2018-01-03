c7d27279008e0d8f3f45a9e9e498fdaf29b19909
Valdis <valdisxp1@gmail.com>
2018-01-03 12:07:22 +0200

    nicer output

--- bin/valdis_tests.sh
+++ bin/valdis_tests.sh
@@ -2,8 +2,10 @@
 
 mkdir -p valdis_data
 i=5
+sbt "run --threads 1 --iterations 1 --format csv"
 for t in {1..40}
 do
   echo "running $i iterations on $t threads"
-  sbt "run --threads $t --iterations $i --format csv" | tee valdis_data/none_gc_${i}_$t.csv
+  sbt "run --threads $t --iterations $i --format csv" > valdis_data/jvm_${i}_$t.log
+  cat valdis_data/jvm_${i}_$t.log | grep montecarlo | cut -d ' ' -f 2 | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | tee valdis_data/jvm_${i}_$t.csv
 done
