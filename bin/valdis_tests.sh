658fa8031d0d7c44438ee24723ebe07aa942a7ff
Valdis <valdisxp1@gmail.com>
2018-01-03 07:28:37 +0200

    automated test

---
+++ bin/valdis_tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p valdis_data
+export SCALANATIVE_GC=none
+sbt rebuild "benchmarks/run --threads 1 --iterations 1 --format csv" | tee valdis_data/build.log
+i=5
+for t in {1..40}
+do
+  echo "running $i iterations on $t threads"
+  benchmarks/target/scala-2.11/benchmarks-out --threads $t --iterations $i --format csv | tee valdis_data/none_gc_${i}_$t.csv
+done
