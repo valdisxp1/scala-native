956bc98a5332b46e72bee4cb24ad841a90c8d98d
Valdis <valdisxp1@gmail.com>
2018-01-03 06:04:43 +0200

    get benchmarks from another branch

---
+++ benchmarks/src/main/scala/montecarlo/PiFuturesBenchmark.scala
@@ -0,0 +1,51 @@
+package montecarlo
+
+import benchmarks.{BenchmarkRunningTime, LongRunningTime}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.forkjoin.ThreadLocalRandom
+
+class PiFuturesBenchmark extends benchmarks.Benchmark[Double] {
+
+  override val runningTime: BenchmarkRunningTime = LongRunningTime
+
+  override def run(): Double = {
+    val points = 200000
+    val futures: Seq[Future[Boolean]] = (1 to points).map { _ =>
+      Future {
+        val random = ThreadLocalRandom.current()
+        val x      = random.nextDouble()
+        val y      = random.nextDouble()
+        x * x + y * y < 1
+      }
+    }
+
+    val countFuture: Future[Int] = Future.fold(futures)(0) { (a, b) =>
+      a + (if (b) 1 else 0)
+    }
+
+    var count = -1
+    val mutex = new Object
+
+    countFuture.foreach { number: Int =>
+      mutex.synchronized {
+        count = number
+        mutex.notifyAll()
+      }
+    }
+
+    if (count == -1) {
+      mutex.synchronized {
+        while (count == -1) {
+          mutex.wait()
+        }
+      }
+    }
+
+    4.0 * count / points
+  }
+
+  override def check(result: Double): Boolean =
+    (Math.PI - result) * (Math.PI - result) < 0.01
+}
---
+++ benchmarks/src/main/scala/montecarlo/PiMultiThreadBenchmark.scala
@@ -0,0 +1,43 @@
+package montecarlo
+
+import java.util.Random
+
+import benchmarks.{BenchmarkRunningTime, ShortRunningTime}
+
+class PiMultiThreadBenchmark extends benchmarks.Benchmark[Double] {
+
+  override val runningTime: BenchmarkRunningTime = ShortRunningTime
+
+  class MonteCarloThread(id: Int, iterations: Int)
+      extends Thread(s"Monte Carlo Pi-$id") {
+    var count = 0
+    override def run() = {
+      val random = new Random(System.currentTimeMillis())
+      count = (1 to iterations).count { _ =>
+        val x = random.nextDouble()
+        val y = random.nextDouble()
+        x * x + y * y < 1
+      }
+    }
+  }
+
+  override def run(): Double = {
+    val points      = 200000
+    val threadCount = Runtime.getRuntime.availableProcessors()
+    val spare       = points % threadCount
+    val threads = (1 to threadCount).map { id =>
+      val toRun = points / threadCount + {
+        // divide spare points to the first threads
+        if (id < spare) 1 else 0
+      }
+      new MonteCarloThread(id, toRun)
+    }
+    threads.foreach(_.start())
+    threads.foreach(_.join())
+    val count = threads.map(_.count).sum
+    4.0 * count / points
+  }
+
+  override def check(result: Double): Boolean =
+    (Math.PI - result) * (Math.PI - result) < 0.01
+}
