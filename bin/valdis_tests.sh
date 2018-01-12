36c0fd87ee4120ad19652eadf3f3cb4248b57bed
Valdis <valdisxp1@gmail.com>
2018-01-12 21:09:43 +0200

    boehm

--- bin/valdis_tests.sh
+++ bin/valdis_tests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 mkdir -p valdis_data
-export SCALANATIVE_GC=none
-sbt rebuild "benchmarks/run --threads 1 --iterations 1 --format csv" | tee valdis_data/build.log
+export SCALANATIVE_GC=boehm
+sbt rebuild "benchmarks/run --threads 1 --iterations 1 --format csv" | tee valdis_data/build-boehm.log
 i=5
 for t in {1..40}
 do
   echo "running $i iterations on $t threads"
-  benchmarks/target/scala-2.11/benchmarks-out --threads $t --iterations $i --format csv | tee valdis_data/none_gc_${i}_$t.csv
+  benchmarks/target/scala-2.11/benchmarks-out --threads $t --iterations $i --format csv | tee valdis_data/boehm_gc_${i}_$t.csv
 done
