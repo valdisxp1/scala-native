5af3d78a219d9f1d1db6b9c1b48f0f712299e223
Valdis <valdisxp1@gmail.com>
2018-01-12 19:25:04 +0100

    until 16

--- bin/valdis_tests.sh
+++ bin/valdis_tests.sh
@@ -4,7 +4,7 @@ mkdir -p valdis_data
 export SCALANATIVE_GC=none
 sbt rebuild "benchmarks/run --threads 1 --iterations 1 --format csv" | tee valdis_data/build.log
 i=5
-for t in {1..40}
+for t in {1..16}
 do
   echo "running $i iterations on $t threads"
   benchmarks/target/scala-2.11/benchmarks-out --threads $t --iterations $i --format csv | tee valdis_data/none_gc_${i}_$t.csv
