a16aa7221a412c9c94b53d8713d647f4d3a3d926
Valdis <valdisxp1@gmail.com>
2018-10-02 20:28:16 +0200

    refactor: Splitting Block_ and BlockHeader_ functions by what is the first argument.

--- nativelib/src/main/resources/gc/immix/Block.h
+++ nativelib/src/main/resources/gc/immix/Block.h
@@ -9,12 +9,12 @@
 
 extern Heap heap;
 
-static inline uint32_t Block_GetBlockIndex(BlockHeader *blockHeader) {
+static inline uint32_t BlockHeader_GetBlockIndex(BlockHeader *blockHeader) {
     return (uint32_t)((word_t *)blockHeader - heap.blockHeaderStart) / WORDS_IN_BLOCK_METADATA;
 }
 
-static inline word_t *Block_GetFirstWord(BlockHeader *blockHeader) {
-    uint32_t index = Block_GetBlockIndex(blockHeader);
+static inline word_t *BlockHeader_GetBlockStart(BlockHeader *blockHeader) {
+    uint32_t index = BlockHeader_GetBlockIndex(blockHeader);
     return heap.heapStart + (WORDS_IN_BLOCK * index);
 }
 
@@ -28,31 +28,31 @@ static inline BlockHeader *Block_GetBlockHeader(word_t *word) {
     return (BlockHeader *)(heap.blockHeaderStart + (index * WORDS_IN_BLOCK_METADATA));
 }
 
-static inline word_t *Block_GetLineAddress(BlockHeader *blockHeader,
+static inline word_t *BlockHeader_GetLineAddress(BlockHeader *blockHeader,
                                            int lineIndex) {
     assert(lineIndex < LINE_COUNT);
-    return Block_GetFirstWord(blockHeader) + (WORDS_IN_LINE * lineIndex);
+    return BlockHeader_GetBlockStart(blockHeader) + (WORDS_IN_LINE * lineIndex);
 }
 
-static inline word_t *Block_GetBlockEnd(BlockHeader *blockHeader) {
-    return Block_GetFirstWord(blockHeader) + (WORDS_IN_LINE * LINE_COUNT);
+static inline word_t *BlockHeader_GetBlockEnd(BlockHeader *blockHeader) {
+    return BlockHeader_GetBlockStart(blockHeader) + (WORDS_IN_LINE * LINE_COUNT);
 }
 
 static inline uint32_t Block_GetLineIndexFromWord(BlockHeader *blockHeader,
                                                   word_t *word) {
-    word_t *firstWord = Block_GetFirstWord(blockHeader);
-    return (uint32_t)((word_t)word - (word_t)firstWord) >> LINE_SIZE_BITS;
+    word_t *blockStart = BlockHeader_GetBlockStart(blockHeader);
+    return (uint32_t)((word_t)word - (word_t)blockStart) >> LINE_SIZE_BITS;
 }
 
-static inline word_t *Block_GetLineWord(BlockHeader *blockHeader, int lineIndex,
+static inline word_t *BlockHeader_GetLineWord(BlockHeader *blockHeader, int lineIndex,
                                         int wordIndex) {
     assert(wordIndex < WORDS_IN_LINE);
-    return &Block_GetLineAddress(blockHeader, lineIndex)[wordIndex];
+    return &BlockHeader_GetLineAddress(blockHeader, lineIndex)[wordIndex];
 }
 
-static inline FreeLineHeader *Block_GetFreeLineHeader(BlockHeader *blockHeader,
+static inline FreeLineHeader *BlockHeader_GetFreeLineHeader(BlockHeader *blockHeader,
                                                       int lineIndex) {
-    return (FreeLineHeader *)Block_GetLineAddress(blockHeader, lineIndex);
+    return (FreeLineHeader *)BlockHeader_GetLineAddress(blockHeader, lineIndex);
 }
 
 void Block_Recycle(Allocator *, BlockHeader *);
