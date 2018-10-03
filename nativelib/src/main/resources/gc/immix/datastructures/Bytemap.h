551388dd943b7c38984b6469711f408755ea9718
Valdis <valdisxp1@gmail.com>
2018-10-03 13:02:38 +0200

    basic bytemap

---
+++ nativelib/src/main/resources/gc/immix/datastructures/Bytemap.h
@@ -0,0 +1,26 @@
+#ifndef IMMIX_BYTEMAP_H
+#define IMMIX_BYTEMAP_H
+
+#include <stddef.h>
+#include "../GCTypes.h"
+#include "../Constants.h"
+
+typedef struct {
+    word_t *firstAddress;
+    uint_32_t *size;
+    uint_32_t *end;
+    ubyte_t data[0];
+} ByteMap;
+
+typedef enum {
+    bm_free = 0x0,
+    bm_allocated = 0x1,
+} Flag;
+
+void Bytemap_Init(Bytemap *bytemap, word_t *firstAddress, word_t *lastAddress);
+ubyte_t Bytemap_Get(Bytemap *bytemap, word_t* address);
+bool Bytemap_IsObject(Bytemap *bytemap, word_t* address);
+void Bytemap_SetFree(Bytemap *bytemap, word_t* address);
+void Bytemap_SetAllocated(Bytemap *bytemap, word_t* address);
+
+#endif // IMMIX_BYTEMAP_H
