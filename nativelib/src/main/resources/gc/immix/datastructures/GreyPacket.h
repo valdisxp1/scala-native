bddb9a41308e9fe70e4a383a737a052ed4c69f12
Valdis <valdisxp1@gmail.com>
2018-12-02 18:28:39 +0100

    grey packets

---
+++ nativelib/src/main/resources/gc/immix/datastructures/GreyPacket.h
@@ -0,0 +1,33 @@
+#ifndef IMMIX_GREYPACKET_H
+#define IMMIX_GREYPACKET_H
+
+#include <stdbool.h>
+#include "Stack.h"
+#include "Constants.h"
+#include "../GCTypes.h"
+
+typedef struct {
+    void *next;
+    word_t size;
+    Stack_Type items[GREY_PACKET_ITEMS];
+} GreyPacket;
+
+#define GREYLIST_LAST ((void *)1)
+
+typedef struct {
+    atomic_uintptr_t head;
+    atomic_uint_fast32_t size;
+} GreyList;
+
+void GreyPacket_Init(GreyPacket *packet);
+bool GreyPacket_Push(GreyPacket *packet, Stack_Type value);
+Stack_Type GreyPacket_Pop(GreyPacket *packet);
+bool GreyPacket_IsEmpty(GreyPacket *packet);
+
+void GreyList_Init(GreyList *list);
+void GreyList_Push(GreyList *list, GreyPacket *packet);
+void GreyList_PushAll(GreyList *list, GreyPacket *first, GreyPacket *last);
+GreyPacket *GreyList_Pop(GreyList *list);
+
+
+#endif // IMMIX_GREYPACKET_H
